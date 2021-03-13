157b2f6faa511401cb02f57588126d4b35c124d0
Kostiantyn <70088981+d1rk-1@users.noreply.github.com>
2021-03-13 22:35:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array.length === 0 || array === undefined || array === null) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array.length === 0 || array === undefined || array === null) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array){ if (array.length === 0 || array === undefined || array === null) {
+        return 0;
+    } else {
+            let sum = array.reduce((a, b) => a + b, 0);
+            let result = sum / array.length;
+            return result;
+        }
+    };
